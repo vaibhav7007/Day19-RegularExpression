84a896851885b72589109746009fcdb128f263d6
vaibhav <vaibhavmehta524@gmail.com>
2023-01-18 11:42:08 +0530

    checking if pin 6 digit or not

---
+++ Regular_Expression/PinCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    class PinCode
+    {
+        public static void check_Six_Digit_Pin_Code()
+        {
+            //UC1 Checking the PIN Code is a 6 digit code 
+            string pattern = "^[0-9]{6}$";
+            string[] pincode = { "400088", "431717", "400085", "400A3b", "A411004" };
+
+            foreach (string input in pincode)
+            {
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Console.WriteLine("{0} is valid ", input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is invalid", input);
+                }
+            }
+        }
+    }
+}
---
+++ Regular_Expression/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the regular expression");
+            PinCode.check_Six_Digit_Pin_Code();
+            Console.ReadLine();
+        }
+    }
+}
